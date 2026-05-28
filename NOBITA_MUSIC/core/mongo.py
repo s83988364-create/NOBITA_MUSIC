237e717e089859ee1e518ddec1f0d238d1ce1a25
s83988364-create <s83988364@gmail.com>
2026-05-29 00:09:19 +0530

    Update mongo.py

--- NOBITA_MUSIC/core/mongo.py
+++ NOBITA_MUSIC/core/mongo.py
@@ -2,33 +2,43 @@ from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
 from pymongo import MongoClient
 from pyrogram import Client
 
+import asyncio
 import config
 
 from ..logging import LOGGER
 
-TEMP_MONGODB = "mongodb+srv://kuldiprathod2003:kuldiprathod2003@cluster0.wxqpikp.mongodb.net/?retryWrites=true&w=majority"
-
+TEMP_MONGODB = "mongodb+srv://f7329984_db_user:bhaskarbaruah@cluster0.xjpwsjd.mongodb.net/?appName=Cluster0"
 
 if config.MONGO_DB_URI is None:
     LOGGER(__name__).warning(
-        "𝐍o 𝐌ONGO 𝐃B 𝐔RL 𝐅ound.. 𝐘our 𝐁ot 𝐖ill 𝐖ork 𝐎n 𝐍𝐎𝐁𝐈𝐓𝐀 𝐌𝐔𝐒𝐈𝐂 𝐃atabase"
+        "No MONGO DB URL Found.. Your Bot Will Work On NOBITA MUSIC Database"
     )
+
     temp_client = Client(
         "NOBITA_MUSIC",
         bot_token=config.BOT_TOKEN,
         api_id=config.API_ID,
         api_hash=config.API_HASH,
     )
-    temp_client.start()
-    info = temp_client.get_me()
+
+    async def client_info():
+        await temp_client.start()
+        info = await temp_client.get_me()
+        await temp_client.stop()
+        return info
+
+    info = asyncio.get_event_loop().run_until_complete(client_info())
     username = info.username
-    temp_client.stop()
+
     _mongo_async_ = _mongo_client_(TEMP_MONGODB)
     _mongo_sync_ = MongoClient(TEMP_MONGODB)
+
     mongodb = _mongo_async_[username]
     pymongodb = _mongo_sync_[username]
+
 else:
     _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
+
     mongodb = _mongo_async_.Yukki
     pymongodb = _mongo_sync_.Yukki
